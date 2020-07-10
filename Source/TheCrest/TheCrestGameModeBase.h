8abf82998aa9d7536271fc5181ae41559751c06b
skuttenkuler <sam.kuttenk@gmail.com>
2020-07-10 11:47:17 -0700

    initial commit, character pawn, movement and look pitch and yaw working, camera class and component complete and compiled

---
+++ Source/TheCrest/TheCrestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheCrestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THECREST_API ATheCrestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
