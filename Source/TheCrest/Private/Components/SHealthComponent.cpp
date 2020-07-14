d2ac6c2c8c5950534bfbe255340d7b564f089c47
skuttenkuler <sam.kuttenk@gmail.com>
2020-07-13 23:12:51 -0700

    added health component

---
+++ Source/TheCrest/Private/Components/SHealthComponent.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "../../Public/Components/SHealthComponent.h"
+
+// Sets default values for this component's properties
+USHealthComponent::USHealthComponent()
+{
+	
+    Health = 100;
+}
+
+
+// Called when the game starts
+void USHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
